561753e9d4825af0e1efb49b074a9fc422f3bed4
Antoine Duchateau <ad@taktik.be>
2015-12-26 16:59:51 +0100

    Improved GUI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,15 +19,11 @@ logoCode.on('gutterClick', function(cm, n) {
 });
 
 var logo = {
-  canvas : null,
-  ctx: null,
+  canvas : null, ctx: null,
   turtle : {
-    ctx: null,
-    x: 300,
-    y: 300,
+    ctx: null, x: 300, y: 300,
     direction: 0, //North
-    pen: true,
-    linecolor: '#ff3333',
+    pen: true, linecolor: '#ff6666',
 
     show: function() {
       $('#turtle').css({ visibility: 'inherit' });
@@ -43,44 +39,35 @@ var logo = {
       } else {
         this.ctx.moveTo(this.x,this.y);
       }
+      this.ctx.stroke();
     },
 
     fw: function(v) {
       var a = (90 - this.direction) * 3.1415926359 / 180;
-
-      var dx = Math.cos(a) * v;
-      var dy = -Math.sin(a) * v;
-
-      this.x += dx;
-      this.y += dy;
-
+      this.x += Math.cos(a) * v;
+      this.y -= Math.sin(a) * v;
       this.drawLine();
-
       logo.sync();
     },
 
     lt: function(v) {
       this.direction -= v;
-
       logo.sync();
     },
 
     set: function(x,y) {
       this.x = x;
       this.y = y;
-
       this.drawLine();
-
       logo.sync();
     },
 
     home: function() {
       this.set(300,300);
       this.direction = 0;
+      logo.sync();
     }
   },
-  procedures : {},
-  main: {},
 
   bind : function() {
     this.canvas = $('canvas')[0];
@@ -107,7 +94,7 @@ var logo = {
       this.ctx.lineWidth = 1;
 
       LogoParser.parse(logoCode.getValue(),{ tracer: { trace: function(e) {
-        console.log(e.type + ':'+e.rule + ' [' + e.location.start.line + ':' + e.location.start.column + ' -> '+ e.location.end.line + ':' + e.location.end.column + ']');
+        //console.log(e.type + ':'+e.rule + ' [' + e.location.start.line + ':' + e.location.start.column + ' -> '+ e.location.end.line + ':' + e.location.end.column + ']');
       }}}) ();
 
       this.ctx.stroke();
@@ -123,7 +110,7 @@ var logo = {
 
   stop: function() {
     //Do nothing
-  },
+  }
 };
 
 var storage = {
@@ -133,11 +120,16 @@ var storage = {
     return (this._db = new PouchDB('turtles'));
   },
 
-  save: function(name,data) {
+  save: function(name,data,overwrite) {
     var that = this;
     that.load(name).then(function (doc) {
-      doc.data = data;
-      that.db().put(doc).then(function () {$('#consoleText').text(''+name+' saved');});
+      if (overwrite) {
+        doc.data = data;
+        that.db().put(doc).then(function () {
+          $('#consoleText').text('' + name + ' saved');
+          setTimeout(function() {$('#consoleText').text('');}, 5000);
+        });
+      }
     }).catch(function () {
       that.db().put({'_id':name, 'data':data}).then(function () {$('#consoleText').text(''+name+' saved');});
     });
@@ -145,6 +137,12 @@ var storage = {
 
   load: function(name) {
     return this.db().get(name);
+  },
+
+  list: function(cb) {
+    this.db().allDocs().then(function(items) {
+      cb(items.rows.map(function(d) {return d.id}));
+    });
   }
 };
 
@@ -155,13 +153,31 @@ function loadLogo() {
 }
 
 function saveLogo() {
-  storage.save($('#storage input').val(), logoCode.getValue());
+  storage.save($('#storage input').val(), logoCode.getValue(), true);
 }
 
 logo.bind();
 logo.sync();
 logo.turtle.show();
 
-//logoCode.setValue('repete 36 [\n\trepete 4 [\n\t\tavance 100\n\t\tgauche 90\n\t]\n\tgauche 10\n]');
+(function($,UI) {
+  UIkit.on('domready.uk.dom', function(){
+    UI.autocomplete($('.uk-autocomplete'), {
+      'source': function (release) {
+        storage.list(function (d) {
+          release(d.map(function (i) {
+            return {title:i, value:i, url:'#'};
+          }));
+        });
+      }
+    });
 
-logoCode.setValue('to spiral :length\r  if :length > 10 [\r\tav :length\r\tgauche 90\r\tspiral :length - 5\r  ]\rend\r\rstep 100');
+    $('#storage input').val('Ex-Spiral');
+    logoCode.setValue('to spiral :length\r  if :length > 10 [\r\tav :length\r\tgauche 90\r\tspiral :length - 5\r  ]\rend\r\rspiral 100');
+  });
+})(jQuery,UIkit);
+
+storage.save('Ex-Spirale','to spiral :length\r  if :length > 10 [\r\tav :length\r\tgauche 90\r\tspiral :length - 5\r  ]\rend\r\rspiral 100', false);
+storage.save('Ex-Arbre','to arbre :length\r  ifelse :length > 10 [\r\tgauche 40\r\tavance :length\r    arbre :length - 25\r    recule :length\r\tdroite 30\r\tavance :length\r    arbre :length - 15\r    recule :length\r    droite 60\r    avance :length\r    arbre :length - 15\r    recule :length\r    gauche 50\r  ] [\r    ga 30 av 5 dr 60 av 5 ga 60 re 5 dr 60 re 5 ga 30\r  ]\rend\r\rpu cs maison \rdr 45 re 80 ga 45\rpd\rrecule 200\ravance 200\rarbre 90\rgauche 20 arbre 85\rdroite 60 arbre 65', false);
+storage.save('Ex-Carres','repete 36 [\n\trepete 4 [\n\t\tavance 100\n\t\tgauche 90\n\t]\n\tgauche 10\n]',false);
+
